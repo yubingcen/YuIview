eb9ab2d6817e79d1fdc1f6b90d4de7e9b6a42e26
yubingcen <644268318@qq.com>
2019-02-14 21:52:22 +0800

    修改布局

--- src/app/desktop/base/layout/Layout.vue
+++ src/app/desktop/base/layout/Layout.vue
@@ -4,6 +4,9 @@
       <Header>
         <Menu mode="horizontal" theme="dark" active-name="1">
           <div class="layout-logo"></div>
+          <div style="float: left; margin-left: 30px;">
+            <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '0 20px'}" type="md-menu" size="24"></Icon>
+          </div>
           <div class="layout-nav">
             <MenuItem name="1">
               <Icon type="ios-navigate"></Icon>
@@ -25,33 +28,20 @@
         </Menu>
       </Header>
       <Layout>
-        <Sider hide-trigger>
-          <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
-            <Submenu name="1">
-              <template slot="title">
-                <Icon type="ios-navigate"></Icon>
-                Item 1
-              </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
-            </Submenu>
-            <Submenu name="2">
-              <template slot="title">
-                <Icon type="ios-keypad"></Icon>
-                Item 2
-              </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
-            </Submenu>
-            <Submenu name="3">
-              <template slot="title">
-                <Icon type="ios-analytics"></Icon>
-                Item 3
-              </template>
-              <MenuItem name="3-1">Option 1</MenuItem>
-              <MenuItem name="3-2">Option 2</MenuItem>
-            </Submenu>
+        <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
+          <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']" :class="menuitemClasses">
+            <MenuItem name="1-1">
+              <Icon type="ios-navigate"></Icon>
+              <span>Option 1</span>
+            </MenuItem>
+            <MenuItem name="1-2">
+              <Icon type="ios-search"></Icon>
+              <span>Option 2</span>
+            </MenuItem>
+            <MenuItem name="1-3">
+              <Icon type="ios-settings"></Icon>
+              <span>Option 3</span>
+            </MenuItem>
           </Menu>
         </Sider>
         <Layout :style="{padding: '0 24px 24px'}">
@@ -70,6 +60,30 @@
 </template>
 <script>
 export default {
+  data () {
+    return {
+      isCollapsed: false
+    }
+  },
+  computed: {
+    rotateIcon () {
+      return [
+        'menu-icon',
+        this.isCollapsed ? 'rotate-icon' : ''
+      ]
+    },
+    menuitemClasses () {
+      return [
+        'menu-item',
+        this.isCollapsed ? 'collapsed-menu' : ''
+      ]
+    }
+  },
+  methods: {
+    collapsedSider () {
+      this.$refs.side1.toggleCollapse()
+    }
+  }
 }
 </script>
 <style scoped>
@@ -104,5 +118,37 @@ export default {
   width: 420px;
   margin: 0 auto;
   margin-right: 20px;
+}
+.menu-icon{
+  color: aliceblue;
+  transition: all .3s;
+}
+.rotate-icon{
+  transform: rotate(-90deg);
+}
+.menu-item span{
+  display: inline-block;
+  overflow: hidden;
+  width: 69px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  vertical-align: bottom;
+  transition: width .2s ease .2s;
+}
+.menu-item i{
+  transform: translateX(0px);
+  transition: font-size .2s ease, transform .2s ease;
+  vertical-align: middle;
+  font-size: 16px;
+}
+.collapsed-menu span{
+  width: 0px;
+  transition: width .2s ease;
+}
+.collapsed-menu i{
+  transform: translateX(5px);
+  transition: font-size .2s ease .2s, transform .2s ease .2s;
+  vertical-align: middle;
+  font-size: 22px;
 }
 </style>
