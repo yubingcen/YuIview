2d1cf0286daf29794976766f1775d2d8d22c08bc
yubingcen <644268318@qq.com>
2019-02-15 10:33:41 +0800

    新增组件

---
+++ src/app/desktop/base/components/XLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <a v-if="isElink" :href="to" :target="target">
+    <slot></slot>
+  </a>
+  <router-link v-else :to="to">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'x-link',
+  props: ['to', 'target'],
+  computed: {
+    isElink () {
+      return typeof this.to === 'string' && this.to.match(/^((http(s)?|ftp):\/\/)|javascript:/)
+    }
+  }
+}
+</script>
+
